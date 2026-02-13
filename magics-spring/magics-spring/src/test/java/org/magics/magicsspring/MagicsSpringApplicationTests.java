895765fe985ce77eefea42fed5b27d68516a114f
leeyang2077 <152938892+leeyang2077@users.noreply.github.com>
2026-02-13 12:36:13 -0800

    Remove Cloudflare Worker (wrangler) and Upstash Redis

---
+++ magics-spring/magics-spring/src/test/java/org/magics/magicsspring/MagicsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.magics.magicsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
